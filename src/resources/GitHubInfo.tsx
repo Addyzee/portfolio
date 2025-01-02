886b18eef24d56f94ffff0a892381f6df7403915
Andrew Kimani <andrewkamaukim@gmail.com>
2025-01-02 12:16:12 +0300

    project 7 added

--- src/resources/GitHubInfo.tsx
+++ src/resources/GitHubInfo.tsx
@@ -5,7 +5,7 @@ const rawBaseURL = `https://raw.githubusercontent.com/Addyzee`;
 const apiURL = `https://api.github.com/users/addyzee/repos`;
 const branchURL = `refs/heads/master`;
 const docsLocation = `PROJECT/DOCS`;
-const reposToInclude = ["campus-network", "risk-awareness-streamlit", "romanian-travel", "ecoverde-scrape", "0X", "spectf-dt"];
+const reposToInclude = ["campus-network", "risk-awareness-streamlit", "romanian-travel", "ecoverde-scrape", "0X", "spectf-dt", "CogSkills-SelfLearn"];
 
 
 export const getRepos = async (): Promise<GitHubRepo[]> => {
